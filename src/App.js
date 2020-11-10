0754c9195963dad561db6306c736d305ddde9edb
AaronY666 <yedongdong2019@163.com>
2020-11-10 23:17:24 +0800

    config redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { GlobalStyle } from "./style";
 import { IconStyle } from "./assets/iconfont/iconfont";
 import routes from "./routes/index.js";
-import { renderRoutes } from 'react-router-config';//renderRoutes 读取路由配置转化为 Route 标签
+import { renderRoutes } from "react-router-config"; //renderRoutes 读取路由配置转化为 Route 标签
 import { HashRouter } from "react-router-dom";
+import store from "./store/index";
+import { Provider } from "react-redux";
 function App() {
   return (
-    <HashRouter>
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      {renderRoutes(routes)}
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        {renderRoutes(routes)}
+      </HashRouter>
+    </Provider>
   );
 }
 
